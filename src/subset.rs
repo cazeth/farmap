f421f33baa42d89e8be2856b011c974d3bfd7eba
cazeth <50043106+cazeth@users.noreply.github.com>
2025-02-10 10:27:55 +0100

    feat(UsersSubset): add methods filter and user_count. Also add tests

--- src/subset.rs
+++ src/subset.rs
@@ -2,6 +2,7 @@ use crate::spam_score::SpamScore;
 use crate::user::User;
 use crate::user::Users;
 use crate::utils::distribution_from_counts;
+use chrono::NaiveDate;
 use std::collections::HashMap;
 
 pub struct UsersSubset<'a> {
@@ -23,6 +24,19 @@ impl<'a> UsersSubset<'a> {
         Self { map: filtered_map }
     }
 
+    /// apply filter to existing subset and mutate subset.
+    pub fn filter<F>(&mut self, filter: F)
+    where
+        F: Fn(&User) -> bool,
+    {
+        self.map = self
+            .map
+            .values()
+            .filter(|user| filter(user))
+            .map(|user| (user.fid(), *user))
+            .collect::<HashMap<usize, &User>>();
+    }
+
     pub fn current_spam_score_distribution(&self) -> Option<[f32; 3]> {
         let mut counts = [0; 3];
         for (_, user) in self.map.iter() {
@@ -34,6 +48,10 @@ impl<'a> UsersSubset<'a> {
         }
 
         distribution_from_counts(&counts)
+    }
+
+    pub fn user_count(&self) -> usize {
+        self.map.len()
     }
 }
 
@@ -56,5 +74,31 @@ mod tests {
             subset.current_spam_score_distribution(),
             Some([0.0, 0.0, 1.0])
         );
+    }
+
+    #[test]
+    fn test_user_count() {
+        let users = Users::create_from_dir("data/dummy-data");
+        let mut subset = UsersSubset::from_filter(&users, |_: &User| true);
+        subset.filter(|user: &User| {
+            !user.created_at_or_after_date(NaiveDate::from_ymd_opt(2023, 12, 29).unwrap())
+        });
+        assert_eq!(subset.user_count(), 0);
+        let mut subset = UsersSubset::from_filter(&users, |_: &User| true);
+        subset.filter(|user: &User| {
+            !user.created_at_or_after_date(NaiveDate::from_ymd_opt(2024, 6, 1).unwrap())
+        });
+        assert_eq!(subset.user_count(), 1);
+    }
+
+    #[test]
+    fn filter_test() {
+        let users = Users::create_from_dir("data/dummy-data");
+        let mut subset = UsersSubset::from_filter(&users, |_: &User| true);
+        assert_eq!(subset.user_count(), 2);
+        subset.filter(|user: &User| user.fid() != 3);
+        assert_eq!(subset.user_count(), 2);
+        subset.filter(|user: &User| user.fid() == 1);
+        assert_eq!(subset.user_count(), 1);
     }
 }
