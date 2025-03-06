c0fb2308a0db09c71e938a26ae318c51318659fc
cazeth <50043106+cazeth@users.noreply.github.com>
2025-03-06 08:28:53 +0100

    feat(api): add weekly spam count route

--- crates/farmap-api/src/main.rs
+++ crates/farmap-api/src/main.rs
@@ -42,6 +42,7 @@ async fn main() {
             get(monthly_spam_score_distributions),
         )
         .route("/weekly_spam_scores", get(weekly_spam_score_distributions))
+        .route("/weekly_spam_scores_counts", get(weekly_spam_score_counts))
         .route("/latest_moves", get(latest_moves))
         .with_state(shared_users)
         .layer(cors_layer);
@@ -129,6 +130,23 @@ async fn fid(
     } else {
         Err(StatusCode::NOT_FOUND)
     }
+}
+
+async fn weekly_spam_score_counts(
+    Query(filters): Query<Filters>,
+    State(users): State<Arc<UserCollection>>,
+) -> Json<Value> {
+    let users_ref: &UserCollection = &users;
+    let mut set = UsersSubset::from(users_ref);
+    if let Some(to_fid) = filters.to_fid {
+        set.filter(|user: &User| user.fid() as u64 <= to_fid);
+    };
+    if let Some(from_fid) = filters.from_fid {
+        set.filter(|user: &User| user.fid() as u64 >= from_fid);
+    };
+
+    let counts = set.weekly_spam_score_counts();
+    Json(json!(counts))
 }
 
 async fn spam_score_distributions_for_cohort(
