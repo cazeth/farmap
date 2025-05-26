4cf74648c0cca26abe37655f2015b10a44a25531
cazeth <50043106+cazeth@users.noreply.github.com>
2025-05-26 10:17:20 +0200

    feat(pinata_importer): add link_request_for_fid and reactions_by_fid
    Also do some refactoring

--- crates/farmap/src/pinata_importer.rs
+++ crates/farmap/src/pinata_importer.rs
@@ -10,13 +10,14 @@ pub struct PinataFetcher {
 impl Default for PinataFetcher {
     fn default() -> Self {
         Self {
-            base_url: Url::parse("https://hub.pinata.cloud/v1/castsByFid").unwrap(),
+            base_url: Url::parse("https://hub.pinata.cloud/v1/").unwrap(),
             client: Client::new(),
         }
     }
 }
 
 impl PinataFetcher {
+    /// used to override the default url - mostly used for testing
     pub fn with_base_url(self, url: Url) -> Self {
         Self {
             base_url: url,
@@ -25,11 +26,34 @@ impl PinataFetcher {
     }
 
     pub async fn api_request_for_id(&self, id: u64) -> Result<Response, ImporterError> {
+        let extension = "castsByFid";
+        let mut url = self.base_url.clone().join(extension).unwrap();
+        url.set_query(Some(&format!("fid={id}")));
         self.client
-            .get(
-                Url::parse_with_params(self.base_url.as_str(), &[("fid", id.to_string())])
-                    .map_err(|_| ImporterError::InvalidEndpoint)?,
-            )
+            .get(url)
+            .send()
+            .await
+            .map_err(|_| ImporterError::FailedApiRequest)
+    }
+
+    pub async fn link_request_for_fid(&self, fid: u64) -> Result<Response, ImporterError> {
+        let extension = "linksByTargetFid";
+        let mut url = self.base_url.clone().join(extension).unwrap();
+        url.set_query(Some(&format!("link_type=follow&target_fid={fid}")));
+        println!("{}", url);
+        self.client
+            .get(url)
+            .send()
+            .await
+            .map_err(|_| ImporterError::FailedApiRequest)
+    }
+
+    pub async fn reactions_by_fid(&self, fid: u64) -> Result<Response, ImporterError> {
+        let extension = "reactionsByFid";
+        let mut url = self.base_url.clone().join(extension).unwrap();
+        url.set_query(Some(&format!("fid={fid}")));
+        self.client
+            .get(url)
             .send()
             .await
             .map_err(|_| ImporterError::FailedApiRequest)
