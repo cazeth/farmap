955a85f6b5a0b53a8591b6630623fb3ede659764
cazeth <50043106+cazeth@users.noreply.github.com>
2025-06-03 09:30:05 +0200

    feat(user): deprecate add_reaction_times

--- crates/farmap/src/user.rs
+++ crates/farmap/src/user.rs
@@ -42,6 +42,9 @@ impl User {
         self.latest_cast_record_check_date = Some(check_date);
     }
 
+    #[deprecated(
+        note = "use update rection times instead, as it prevents acccidentally overriding old values"
+    )]
     pub fn add_reaction_times(&mut self, reaction_times: Vec<NaiveDateTime>) {
         self.reaction_times = Some(reaction_times);
     }
