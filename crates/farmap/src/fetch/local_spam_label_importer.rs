61e7253a9d22e9c828f52eca6073488b172273a8
cazeth <50043106+cazeth@users.noreply.github.com>
2025-06-23 10:13:14 +0200

    feat(local_spam_label_importer): create fn import_data_from_dir_with_collected_res
    Add tests.
    
    Also broaden import_data_from_file_with_collected_res to accept impl
    AsRef<Path>

--- crates/farmap/src/fetch/local_spam_label_importer.rs
+++ crates/farmap/src/fetch/local_spam_label_importer.rs
@@ -7,20 +7,51 @@ use crate::UnprocessedUserLine;
 use itertools::Itertools;
 use serde_jsonlines::json_lines;
 use std::fs::read_dir;
+use std::path::Path;
 
 pub fn import_data_from_file_with_collected_res(
-    path: &str,
+    path: impl AsRef<Path>,
 ) -> Result<Vec<Result<UnprocessedUserLine, InvalidJsonlError>>, DataReadError> {
-    Ok(json_lines::<UnprocessedUserLine, _>(path)
+    let path_ref = path.as_ref();
+    Ok(json_lines::<UnprocessedUserLine, _>(&path)
         .map_err(|_| DataReadError::InvalidDataPathError {
-            path: path.to_owned(),
+            path: path_ref.to_str().unwrap().to_string(),
         })?
         .map(|x| {
             x.map_err(|_| InvalidJsonlError {
-                path: "test".to_string(),
+                path: path_ref.to_str().unwrap_or_default().to_string(),
             })
         })
         .collect::<Vec<_>>())
+}
+
+pub fn import_data_from_dir_with_collected_res(
+    path: impl AsRef<Path>,
+) -> Result<Vec<Result<UnprocessedUserLine, InvalidJsonlError>>, DataReadError> {
+    let path_ref = path.as_ref();
+
+    let paths = read_dir(&path).map_err(|_| DataReadError::InvalidDataPathError {
+        path: path_ref.to_str().unwrap_or_default().to_string(),
+    })?;
+    dbg!(&paths);
+    let file_results = paths
+        .flatten()
+        .filter(|paths| paths.path().extension().unwrap_or_default() == "jsonl")
+        .map(|path| import_data_from_file_with_collected_res(path.path()))
+        .collect_vec();
+
+    let mut result: Vec<Result<UnprocessedUserLine, InvalidJsonlError>> = Vec::new();
+
+    for file_result in file_results {
+        if let Ok(mut file_result) = file_result {
+            result.append(&mut file_result);
+        } else {
+            return Err(DataReadError::InvalidDataPathError {
+                path: path_ref.to_str().unwrap_or_default().to_string(),
+            });
+        }
+    }
+    Ok(result)
 }
 
 pub fn import_data_from_dir_with_res(
@@ -88,5 +119,20 @@ pub mod tests {
             Err(_) => panic!(),
             Ok(_) => panic!(),
         }
+    }
+
+    #[test]
+    pub fn test_error_collect_on_invalid_jsonl() {
+        let result = import_data_from_dir_with_collected_res("data/invalid-data/");
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap().iter().filter(|x| x.is_err()).count(), 1);
+    }
+
+    #[test]
+    pub fn test_valid_jsonl() {
+        let result = import_data_from_dir_with_collected_res("data/dummy-data/");
+        println!("{result:?}");
+        assert!(result.is_ok());
+        assert!(result.unwrap().iter().all(|x| x.is_ok()));
     }
 }
