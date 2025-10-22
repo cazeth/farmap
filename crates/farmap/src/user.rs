b329033efbd27fab03c370bea35db9beafcfefb6
cazeth <50043106+cazeth@users.noreply.github.com>
2025-10-22 10:40:04 +0200

    feat(User): create methods
    These methods are necessary since User is moving towards
    genererically handing UserValues instead of concrete types. This makes
    the struct simpler and more scalable. Methods related to particular user
    values will be in other structs, such as UserWithSpamData, where the
    User is guaranteed to have data of that particular type.
    
    Create public methods has, user_values_of_kind, try_add_user_value,
    add_user_value.
    
    Create private method update_time_if_duplicate.
    
    Create associated private function now.

--- crates/farmap/src/user.rs
+++ crates/farmap/src/user.rs
@@ -1,9 +1,11 @@
 use crate::cast_meta::CastMeta;
+use crate::collidable::Collidable;
 use crate::spam_score::SpamEntries;
 use crate::spam_score::SpamEntry;
 use crate::spam_score::SpamScore;
 use crate::user_value::AnyUserValue;
 use crate::UnprocessedUserLine;
+use crate::UserValue;
 use chrono::Datelike;
 use chrono::Local;
 use chrono::NaiveDate;
@@ -47,8 +49,89 @@ impl User {
         }
     }
 
+    /// Check if a User has at least one value of type T.
+    pub fn has<T: UserValue>(&self) -> bool {
+        if let Some(user_values) = &self.user_values {
+            user_values
+                .iter()
+                .any(|(val, _)| T::from_any_user_value_ref(val).is_some())
+        } else {
+            false
+        }
+    }
+
+    /// Add a [`UserValue`] to a [`User`]. Returns an error in case of Collision. This method
+    /// relies on Ts implementation of Collidable to determine collisions.
+    pub fn try_add_user_value<T>(&mut self, value: T) -> Result<(), UserValueError>
+    where
+        T: UserValue + Collidable,
+    {
+        if self
+            .user_values_of_kind::<T>()
+            .iter()
+            .all(|x| !Collidable::is_collision(*x, &value))
+        {
+            self.add_user_value(value)
+        } else {
+            Err(UserValueError::CollisionError)
+        }
+    }
+
+    /// Insert a new [`UserValue`]. Returns an error on collision. Returns an Ok on duplicate.
+    pub fn add_user_value<T>(&mut self, value: T) -> Result<(), UserValueError>
+    where
+        T: UserValue,
+    {
+        if self.update_time_if_duplicate(&value).is_some() {
+            return Ok(());
+        };
+
+        let any_user_value = value.into_any_user_value();
+        if let Some(value_vec) = &mut self.user_values {
+            value_vec.push((any_user_value, Self::now()))
+        } else {
+            self.user_values = Some(vec![(any_user_value, Self::now())]);
+        }
+        Ok(())
+    }
+
     pub fn all_user_values(&self) -> &Option<Vec<(AnyUserValue, NaiveDateTime)>> {
         &self.user_values
+    }
+
+    /// Get all the [`UserValue`]s of type T. If there are no such values, the method returns an
+    /// empty vec.
+    pub fn user_values_of_kind<T>(&self) -> Vec<&T>
+    where
+        T: UserValue,
+    {
+        if let Some(user_values) = &self.user_values {
+            user_values
+                .iter()
+                .flat_map(|(user_value, _)| user_value.specify_ref::<T>())
+                .collect_vec()
+        } else {
+            Vec::new()
+        }
+    }
+    fn now() -> NaiveDateTime {
+        Local::now().naive_local()
+    }
+    fn update_time_if_duplicate<T>(&mut self, value: &T) -> Option<NaiveDateTime>
+    where
+        T: UserValue,
+    {
+        if let Some(user_values) = &mut self.user_values {
+            let duplicate_position = user_values
+                .iter()
+                .position(|prev| prev.0 == value.as_any_user_value())?;
+
+            let new_time = Self::now();
+            let old_time = std::mem::replace(&mut user_values[duplicate_position].1, new_time);
+            Some(old_time)
+        } else {
+            None
+        }
     }
     pub fn new_without_labels(fid: usize) -> Self {
         Self {
