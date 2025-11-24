4c0b837bb8f99b99ecbb9615078f12d46429204f
cazeth <50043106+cazeth@users.noreply.github.com>
2025-11-24 16:28:04 +0100

    refactor(UserError): remove ThisError dependency

--- crates/farmap/src/user_error.rs
+++ crates/farmap/src/user_error.rs
@@ -1,10 +1,13 @@
-use thiserror::Error;
-
-#[derive(Error, Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 #[non_exhaustive]
 pub enum UserError {
-    #[error(
-        "User Value collides with existing user value. A User cannot contain colliding user values"
-    )]
     CollisionError,
 }
+
+impl std::fmt::Display for UserError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "User Value collides with existing user value. A User cannot contain colliding user values")
+    }
+}
+
+impl std::error::Error for UserError {}
